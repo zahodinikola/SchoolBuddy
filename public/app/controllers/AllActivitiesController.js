0c98d8e4a9b03a5372c87636185c8c672c26fd1e
zahodinikola <zahodinikola@gmail.com>
2017-08-03 06:52:01 +0300

    AllActivitiesController mended

--- public/app/controllers/AllActivitiesController.js
+++ public/app/controllers/AllActivitiesController.js
@@ -2,29 +2,30 @@
 
     angular.module('app')
         .controller('AllActivitiesController',
-            ['$log', '$state', 'dataService', 'notifier', 'activities', AllActivitiesController]);
+                ['$log', 'dataService', AllActivitiesController]);
+            // ['$log', '$state', 'dataService', 'notifier', 'activities', AllActivitiesController]);
 
-    function AllActivitiesController($log, $state, dataService, notifier, activities) {
+    function AllActivitiesController($log, dataService) {//$state, notifier, activities) {
 
         var vm = this;
 
         vm.selectedMonth = 1; // default to January
 
-        vm.allActivities = activities;
+        //vm.allActivities = activities;
 
-        $log.debug($state.current.data);
+        // $log.debug($state.current.data);
 
-        vm.search = function() {
-          $state.go('classroom_detail', {
-              id: vm.selectedClassroom.id,
-              month: vm.selectedMonth
-          });
-        };
+        // vm.search = function() {
+        //   $state.go('classroom_detail', {
+        //       id: vm.selectedClassroom.id,
+        //       month: vm.selectedMonth
+        //   });
+        // };
 
-        dataService.getAllClassrooms()
-            .then(function(classrooms) {
-                vm.allClassrooms = classrooms;
-                vm.selectedClassroom = classrooms[0];
+        dataService.getAllActivities()
+            .then(function(activities) {
+                vm.allActivities = activities;
+                //vm.selectedClassroom = classrooms[0];
             })
             .catch(showError);
 
