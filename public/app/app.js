2d18c75ff7ce3757ea244d38707c3898c367a127
zahodinikola <zahodinikola@gmail.com>
2017-08-16 06:44:00 +0300

    same content after trying reset

--- public/app/app.js
+++ public/app/app.js
@@ -1,87 +1,49 @@
-'use strict';
+(function(){
 
-var app = angular.module('app', ['ngRoute']);
+	var app = angular.module('app', ['ui.router']);
 
-app.config(['$logProvider', '$routeProvider', '$locationProvider', function ($logProvider, $routeProvider, $locationProvider) {
-    
-	$logProvider.debugEnabled(true);
-	
-//	$locationProvider.hashPrefix('!');
+	app.config(['$logProvider', '$stateProvider', function ($logProvider, $stateProvider) {
+		
+		$logProvider.debugEnabled(true);
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'app/templates/home.html',
+				controller: 'HomeController',
+				controllerAs: 'home'
+			})
+			.state('schools', {
+				url: '/schools',
+				templateUrl: 'app/templates/allSchools.html',
+				controller: 'AllSchoolsController',
+				controllerAs: 'schools'
+			})
+			.state('classrooms', {
+				url: '/classrooms',
+				templateUrl: 'app/templates/allClassrooms.html',
+				controller: 'AllClassroomsController',
+				controllerAs: 'classrooms'
+			})
+			.state('activities', {
+				url: '/activities',
+				templateUrl: 'app/templates/allActivities.html',
+				cintroller: 'AllActivitiesController',
+				controllerAs: 'activities'
+			});
+	}]);
 
-	// $locationProvider.html5Mode({
-	// 	enabled: true,
-	// 	requireBase: true,
-	// 	rewriteLinks: true
-	// });
-	
-	$locationProvider.html5Mode(true);
+	app.run(['$log', '$rootScope', function($rootScope, $log) {
 
-    $routeProvider
-        .when('/', {
-            controller: 'HomeController',
-            controllerAs: 'home',
-            templateUrl: 'app/templates/home.html'
-        })
-		.when('/schools', {
-			controller: 'AllSchoolsController',
-			controllerAs: 'schools',
-			templateUrl: '/app/templates/allSchools.html',
-/* 			caseInsensitiveMatch: true */
-		})
-		.when('/classrooms', { /* /:id', { */
-			controller: 'AllClassroomsController',
-			controllerAs: 'classrooms',
-			templateUrl: 'app/templates/allClassrooms.html',
-			// resolve: {
-			// 	promise: function() {
-			// 		throw "an error transitioning to classrooms";
-			// 	}
-			// }
-/* 															,
-			redirectTo: function(params, currPath, currSearch) {
-				console.log(params);
-				console.log(currPath);
-				console.log(currSearch);
-				return '/';
-			} */
-		})
-		.when('/activities', {
-			controller: 'AllActivitiesController',
-			controllerAs: 'activities',
-			templateUrl: '/app/templates/allActivities.html',
-			resolve: {
-				activities: function(dataService) {
-					return dataService.getAllActivities();
-				}
-			}
-		})
-		.when('/classrooms/:id', {
-			controller: 'ClassroomController',
-			controllerAs: 'classroom',
-			templateUrl: 'app/templates/classroom.html'
-		})
-		.when('/classrooms/:id/detail/:month?', {
-			controller: 'ClassroomController',
-			controllerAs: 'classroom',
-			templateUrl: '/app/templates/classroomDetail.html'
-		})
-		.otherwise('/');
-}]);
+		$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+			
+			$log.debug('successfully changed state');
 
-app.run(['$rootScope', '$log', function($rootScope, $log) {
+			$log.debug('event', event);
+			$log.debug('toState', toState);
+			$log.debug('toParams', toParams);
+			$log.debug('fromState', fromState);
+			$log.debug('fromParams', fromParams);
 
-	$rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-		console.log('successfully changed route');
-		console.log(event);
-		console.log(current);
-		console.log(previous);
-	});
-
-	$rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
-		console.log('error changing route');
-		console.log(event);
-		console.log(current);
-		console.log(previous);
-		console.log(rejection);
-	});
-}]);
+		});
+	}]);
+}());
